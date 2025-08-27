d5d64a62a2331131b0a2d3c5e1cc5f2c632f4660
Arthur Witt <168667004+HeyTenebrisVenator@users.noreply.github.com>
2025-08-27 16:48:48 -0300

    Update script.js

--- Site/script.js
+++ Site/script.js
@@ -1,7 +1,7 @@
 async function send_URL() {
     try {
         const info = document.getElementById("inp").value;
-        const response = await fetch("http://127.0.0.1:5000/send_url?data=" + encodeURIComponent(info));
+        const response = await fetch("http://fndetector.com/send_url?data=" + encodeURIComponent(info));
         document.getElementById("h1style").textContent ="Processando..."
         document.getElementById("h1style").style ="color: rgb(203, 115, 0);"
         
@@ -23,7 +23,7 @@ async function send_Title() {
         const info = document.getElementById("inp").value;
         document.getElementById("h1style").textContent ="Processando..."
         document.getElementById("h1style").style ="color: rgb(203, 115, 0);"
-        const response = await fetch("http://127.0.0.1:5000/send_title?data=" + encodeURIComponent(info));
+        const response = await fetch("http://fndetector.com/send_title?data=" + encodeURIComponent(info));
         
         if (!response.ok) {
             document.getElementById("h1style").textContent ="ERRO"
@@ -35,4 +35,5 @@ async function send_Title() {
             document.getElementById("h1style").innerHTML = "<h1 id='h1style' style='color: rgb(0, 0, 0);'>" + data + "</h1>"    } catch (error) {
         console.error("Erro:", error);
     }
+
 }
