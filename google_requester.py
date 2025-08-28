087a691c4aeb7a54e951c485e220f8b57af31620
Arthur Witt <168667004+HeyTenebrisVenator@users.noreply.github.com>
2025-08-27 21:36:37 -0300

    Update google_requester.py

--- google_requester.py
+++ google_requester.py
@@ -1,8 +1,8 @@
 import requests
 
-API_KEY = 'AIzaSyD6cZy1_J5N-O97bhcX6aqqKbP3_wqfk58'
+API_KEY = #OCULTADO#
 SEARCH_ENGINE_ID = '30854cad23c464de8'
-query = 'Pedido de asilo à Argentina de Milei? A repercussão do indiciamento'.replace(" ", '+')
+query = ''.replace(" ", '+')
 
 url = f'https://www.googleapis.com/customsearch/v1?q={query}&key={API_KEY}&cx={SEARCH_ENGINE_ID}'
 
@@ -10,4 +10,5 @@ response = requests.get(url)
 data = response.json()
 
 for item in data.get('items', []):
+
     print(item['link'])
